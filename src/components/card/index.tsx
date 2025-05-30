43748eb6be43197c844b634d455f6706ea3de9d2
Heng <songhengthoem99@gmail.com>
2025-05-30 23:25:37 +0700

    fix: - card layout - add link icons pending: - improve seo - improve word

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,35 +1,69 @@
-import { ReactElement } from "react";
+import { Fragment, ReactNode } from "react";
 import styles from "./card.module.css";
 
-interface CardProps {
-  type: "project" | "experience";
-  title?: string;
-  company?: string;
-  role?: string;
-  description?: string;
-  footer?: ReactElement;
-  image?: ReactElement;
-}
-
-const Card = (props: CardProps) => {
+const Card = ({
+  children,
+  inline,
+}: {
+  children?: ReactNode;
+  inline?: boolean;
+}) => {
   return (
-    <div className={styles.card_container}>
-      <div className={styles.card_header}>
-        {props.type === "experience" ? <p>{props.title}</p> : props.image}
-      </div>
-
-      <div className={styles.card_content}>
-        <div className={styles.card_title}>
-          <h4>{props.role}</h4>
-          <h5>{props.company}</h5>
-        </div>
-        <div className={styles.card_description}>
-          <p>{props.description}</p>
-        </div>
-        <div className={styles.card_footer}>{props.footer}</div>
-      </div>
+    <div className={`${styles.card_container} ${inline && styles.inline}`}>
+      {children}
     </div>
   );
+};
+
+Card.Header = ({ children }: { children?: ReactNode }) => {
+  return <div className={styles.card_header}>{children && children}</div>;
+};
+
+Card.Content = ({ children }: { children?: ReactNode }) => {
+  return <div className={styles.card_content}>{children && children}</div>;
+};
+
+Card.Title = ({
+  title,
+  subTitles,
+}: {
+  title?: string;
+  subTitles?: string[];
+}) => {
+  return (
+    <div className={styles.card_title}>
+      <h4>{title}</h4>
+
+      {subTitles && subTitles.length > 0 && (
+        <h5>
+          {subTitles.map((subTitle: string, index: number) => {
+            return (
+              <Fragment key={index}>
+                {subTitle}
+                {!(subTitles.length === index + 1) && (
+                  <span>
+                    <div className={styles.seperator} />
+                  </span>
+                )}
+              </Fragment>
+            );
+          })}
+        </h5>
+      )}
+    </div>
+  );
+};
+
+Card.Description = ({ description }: { description?: string }) => {
+  return (
+    <div className={styles.card_description}>
+      {description && <p>{description}</p>}
+    </div>
+  );
+};
+
+Card.Footer = ({ children }: { children?: ReactNode }) => {
+  return <div className={styles.card_footer}>{children}</div>;
 };
 
 export default Card;
